8c9847cd28ac2564b0d0de54eb0e11634463ee98
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-07 12:52:53 -0500

    JS - loops - basic

---
+++ basic3.js
@@ -0,0 +1,21 @@
+// Write a while loop to print the numbers 1 through 10.
+
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+// Write a while loop that prints the word "hello" 5 times.
+let hello = 0;
+while (hello < 5) {
+  console.log("hello");
+  hello++;
+}
+
+// Write a while loop that prints the numbers 0 through 100, increasing by 5 each time
+let num = 0;
+while (num <= 100) {
+  console.log(num);
+  num = num + 5;
+}
